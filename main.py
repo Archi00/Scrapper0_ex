8490ea3166c382e106ca739de9543fe9aae6a1d0
Archi00 <archi00@localhost.localdomain>
2021-03-27 22:47:37 +0100

    Changed to txt format only links, vids inc

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from bs4 import BeautifulSoup as soup
-import urllib.request as req
+from urllib import request as req
 
 my_url = req.urlopen('https://www.reddit.com/r/Awww/')
-fd = open('./images.html', 'w')
+o_file = open('./images.txt', 'w')
 
 def main():
-  page = soup(my_url, 'html.parser')
-  images = page.findAll(['img', 'src'])
-  print(images['alt'])
+    page = soup(my_url, 'html.parser')
+    images = page.findAll(attrs={'alt': 'Post image'})
+    vids = page.findAll('video', class_='_1EQJpXY7ExS04odI1YBBlj')
 
-  for each in images:
-    url = each['src']
-    fd.write("""
-          <div>
-            <img alt="" src="{}"
-          </div>
-        """.format(url))
-  fd.close()
+    for image in images:
+        print()
+        print(image['src'])
+        
+        o_file.write('[IMAGE]\n{}\n\n'.format(image['src']))
+    
+    o_file.write('\n\n')
 
+    for vid in vids:
+        print()
+        print(vid.source['src'])
+
+        o_file.write('[VID]\n{}\n\n'.format(vid['src']))
+        
+
+    o_file.close()
+     
 if __name__ == "__main__":
   main()
